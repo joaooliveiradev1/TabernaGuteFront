6aa4fffb7643c56ec26bf842163eddc3338ae489
joaooliveiradev1 <joaooliveiradev1@gmail.com>
2025-06-13 15:43:05 -0300

    Fix: Desenvolvendo front

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,19 +2,21 @@
 export function Navbar(){
     
      return (
-        <section className="bg-gray-900 flex justify-between h-18 items-center">
-            <h1 className="text-yellow-400 font-bold text-xl pl-60">Taberna do Gute</h1>
-            <nav className="flex justify-between pr-20 pl-50">
-                <div className="text-gray-100 flex justify-between ">
-                    <li className="pr-5 pl-5">Início</li>
-                    <li className="pr-5 pl-5">Cardápio</li>
-                    <li className="pr-5 pl-5">Sobre</li>
-                    <li className="pr-30 pl-5">Galeria</li>
-                </div>
-                <div className="text-black bg-yellow-300 rounded-md mr-60">
-                    <li>Reservar mesa</li>
-                </div>
-            </nav>
-        </section>
-    );
+    <header className="bg-[#0B0E17] text-white h-16 flex items-center justify-between px-16">
+
+      <h1 className="text-yellow-400 font-bold text-lg">Taberna do Gute</h1>
+
+      <nav className="flex items-center gap-10">
+        <ul className="flex gap-6 text-sm font-medium">
+          <li className="hover:text-yellow-400 cursor-pointer">Início</li>
+          <li className="hover:text-yellow-400 cursor-pointer">Cardápio</li>
+          <li className="hover:text-yellow-400 cursor-pointer">Sobre</li>
+          <li className="hover:text-yellow-400 cursor-pointer">Galeria</li>
+        </ul>
+        <button className="bg-#FFED65 text-black text-sm px-4 py-2 rounded-md font-semibold hover:bg-yellow-400 transition">
+          Reservar Mesa
+        </button>
+      </nav>
+    </header>
+  );
 }
--- src/app/components/Sobre.tsx
+++ src/app/components/Sobre.tsx
@@ -1,42 +1,44 @@
 export function Sobre(){
      return (
-        <section>
-            <h1>Sobre nós</h1>
-            <div>
-                <p>
-                    A Taberna do Gute nasceu da paixão do Chef Artur Schmidt por unir 
-                    suas raízes alemãs com os sabores vibrantes do Brasil. Localizado em 
-                    Horizonte, Ceará, nosso restaurante oferece uma experiência 
-                    gastronômica única que celebra essa fusão cultural.
+       <section className="flex flex-col gap-8 px-8 max-w-screen-xl mx-auto">
+           <h1 className="text-4xl font-bold text-center text-gray-900 pt-12 mb-12 relative inline-block after:content-[''] after:block after:h-1 after:w-16 after:bg-blue-800 after:mx-auto after:mt-2">
+                Sobre nós
+            </h1>
 
-                    Nossa proposta é trazer para você o melhor da culinária alemã
-                    tradicional com toques da rica gastronomia brasileira, criando uma
-                    experiência única para o seu paladar.
-
-                    Em um ambiente aconchegante e elegante, convidamos você a
-                    desfrutar de pratos cuidadosamente elaborados, acompanhados de
-                    cervejas artesanais e uma seleção de vinhos que complementam
-                    perfeitamente nossa culinária.
+            <div className="flex flex-col lg:flex-row gap-12">
+                <div className="flex-1">
+                <p className="mb-4">
+                    A Taberna do Gute nasceu da paixão do Chef Artur Schmidt por unir suas raízes alemãs com os 
+                    sabores vibrantes do Brasil. Localizado em Horizonte, Ceará, nosso restaurante oferece uma experiência gastronômica única que celebra essa fusão cultural.
                 </p>
-            </div>
-            <div>
-                <div>
-                    <h3>Gastronomia</h3>
-                    <p>Fusão da culinária alemã com ingredientes brasileiros</p>
+                <p className="mb-4">
+                    Nossa proposta é trazer para você o melhor da culinária alemã tradicional com toques da rica gastronomia
+                    brasileira, criando uma experiência única para o seu paladar.
+                </p>
+                <p>
+                Em um ambiente aconchegante e elegante, convidamos você a desfrutar de pratos cuidadosamente 
+                elaborados, acompanhados de cervejas artesanais e uma seleção de vinhos que complementam perfeitamente nossa culinária.
+                </p>
                 </div>
-                <div>
-                    <h3>Bebidas</h3>
-                    <p>Cervejas artesanais e vinhos selecionados</p>
+                <div className="grid grid-cols-2 gap-4 self-center">
+                <div className="flex flex-col items-center justify-center text-center h-40 w-56 bg-green-50 rounded-xl p-4">
+                    <h3 className="font-semibold">Gastronomia</h3>
+                    <p className="text-sm">Fusão da culinária alemã com ingredientes brasileiros</p>
                 </div>
-                <div>
-                    <h3>Ambiente</h3>
-                    <p>Aconchegante e elegante para todas as ocasiões</p>
+                <div className="flex flex-col items-center justify-center text-center h-40 w-56 bg-indigo-50 rounded-xl p-4">
+                    <h3 className="font-semibold">Bebidas</h3>
+                    <p className="text-sm">Cervejas artesanais e vinhos selecionados</p>
                 </div>
-                <div>
-                    <h3>Experiência</h3>
-                    <p>Atendimento personalizado e de alta qualidade</p>
+                <div className="flex flex-col items-center justify-center text-center h-40 w-56 bg-yellow-50 rounded-xl p-4">
+                    <h3 className="font-semibold">Ambiente</h3>
+                    <p className="text-sm">Aconchegante e elegante para todas as ocasiões</p>
+                </div>
+                <div className="flex flex-col items-center justify-center text-center h-40 w-56 bg-rose-50 rounded-xl p-4">
+                    <h3 className="font-semibold">Experiência</h3>
+                    <p className="text-sm">Atendimento personalizado e de alta qualidade</p>
+                </div>
                 </div>
             </div>
         </section>
-      );
+    );
 }
